67aa9d93fabf23c1e45abe2fc64bd505f16cb550
Camden <camden.macleod@pm.me>
2021-11-16 14:53:57 -0500

    Create UI

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,15 +1,17 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
-{
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow) {
     ui->setupUi(this);
+    ui->textEdit->setText("hizies");
 }
 
-MainWindow::~MainWindow()
-{
+MainWindow::~MainWindow() {
     delete ui;
+}
+
+
+void MainWindow::on_pushButton_clicked() {
+
 }
 
